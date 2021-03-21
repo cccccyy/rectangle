cbb8af61544990bacd2afcea7e3485a57c8b134f
蔡清怡 <44658612+cccccyy@users.noreply.github.com>
2021-03-21 12:32:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,60 @@
-const electron = require('electron');
+$(() => {
+  let $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
+      isPassValidate = false;
 
-const app = electron.app;
+  $forkMeGH.show('https://github.com/wangding/rectangle');
+  $bszPageFooter.show('body');
 
-const BrowserWindow = electron.BrowserWindow;
-
-const path = require('path');
-const url = require('url');
-
-let mainWindow;
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width:  800,
-    height: 500,
-    show:   false,
-    title:  '矩形计算器'
+  $width.focusout(() => {
+    let result = validate($width.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $widthValidate.html('宽度' + result.reason);
+      $width.select();
+    } else {
+      $widthValidate.html('');
+    }
   });
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
-
-  mainWindow.on('ready-to-show', function() { 
-    mainWindow.show();
-    mainWindow.focus();
+  $width.keypress((e) => {
+    if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+      e.preventDefault();
+    }
   });
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
+  $height.focusout(() => {
+    let result = validate($height.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $heightValidate.html('高度' + result.reason);
+      $height.select();
+    } else {
+      $heightValidate.html('');
+    }
   });
-}
 
-app.on('ready', createWindow);
+  $height.keypress(function(e) {
+    if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+      e.preventDefault();
+    }
+  });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  $btnCal.click(() => {
+    if(!isPassValidate) return;
+
+    let w = $width.val(),
+        h = $height.val();
+
+    let r = new Rectangle(w, h);
+
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
+  });
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
